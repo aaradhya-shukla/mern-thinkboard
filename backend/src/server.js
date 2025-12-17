9d7feba4402e1b040f0ccde83c865944769472a2
= <=>
2025-12-17 23:12:53 +0530

    prepared for deployement

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,23 +1,38 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import notesRouter from './routes/notes.Router.js'
-import connectToDb from './configs/connectMongodb.js';
-import RateLimiter from "./middlewares/rateLimiter.js"
-import cors from 'cors';
+import express from "express";
+import dotenv from "dotenv";
+import notesRouter from "./routes/notes.Router.js";
+import connectToDb from "./configs/connectMongodb.js";
+import RateLimiter from "./middlewares/rateLimiter.js";
+import cors from "cors";
+import path from "path";
 
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 5001;
+const _dirname = path.resolve();
 
-app.use(cors({
-    origin: "http://localhost:5173"
-}))
+if(process.env.NODE_ENV == 'DEV'){
+    app.use(
+        cors({
+          origin: "http://localhost:5173",
+        })
+      );
+}
 app.use(RateLimiter);
 app.use(express.json());
 
-app.use('/api/notes', notesRouter);
+app.use("/api/notes", notesRouter);
+
+if (process.env.NODE_ENV === "PROD") {
+  app.use(express.static(path.join(_dirname, "../frontend/dist")));
+
+  app.use("*", (_, res) => {
+    res.sendFile(path.join(_dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 connectToDb().then(() => {
-    app.listen(PORT,() => {
-        console.log("server is running on port: ",PORT)
-    })
-})
+  app.listen(PORT, () => {
+    console.log("server is running on port: ", PORT);
+  });
+});
